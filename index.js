65864f9c97820676e25534eed6c246d5ffa43729
AshwinPanicker <107415263+AshwinPanicker@users.noreply.github.com>
2024-03-22 17:44:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const cors = require('cors');
+const app = express();
+const port = 3006;
+
+app.use(cors());
+
+app.listen(port,()=>{
+    console.log(" app listening")
+});
+
+
+app.get('/',(req,res)=>{
+    res.send("hello everyone")
+});
+
+app.get('/add',(req,res)=>{
+    const num1 = req.query.num1;
+    const num2 = req.query.num2
+    if(num1 ==undefined || num2 == undefined){
+        res.status(400).json({
+            message: "Invalid Request"
+        })
+        return;
+    }
+
+    const sum = parseInt(num1) + parseInt(num2);
+    res.json({
+        message: "Status successful",
+        result: sum
+    })
+});
+
+
+app.get('/random',(req,res)=>{
+    const r = Math.random();
+    res.json({
+        message: "Succesful",
+        result: r
+    })
+})
